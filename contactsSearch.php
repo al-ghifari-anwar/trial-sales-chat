25cefa8c763a01a67528e9c505e69da985ab37e4
Al Ghifari <alghifari.anwar2002@gmail.com>
2023-06-22 16:12:17 +0700

    Search contact

---
+++ contactsSearch.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+include_once("config.php");
+
+$wa_token = 'X1CWu_x9GrebaQUxyVGdJF3_4SCsVW9z1QjX-XJ9B6k';
+$template_id = 'b47daffc-7caf-4bea-9f36-edf4067b2c08';
+$integration_id = '31c076d5-ac80-4204-adc9-964c9b0c590b';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['key'])) {
+        $key = $_POST['key'];
+
+        $result = mysqli_query($conn, "SELECT * FROM tb_contact WHERE nama = '$key'");
+
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $transArray[] = $row;
+        }
+
+        mysqli_close($conn);
+
+        echo json_encode(array("status" => "ok", "result" => $transArray));
+    } else {
+
+        echo json_encode(array("status" => "failed", "results" => "null"));
+    }
+}
