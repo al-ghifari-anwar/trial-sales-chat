aa6edeb2e1f871f57ccde218b3dd4404ededb921
Al Ghifari <alghifari.anwar2002@gmail.com>
2023-09-22 12:07:30 +0700

    Data payment

---
+++ payment.php
@@ -0,0 +1,25 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+include_once("config.php");
+date_default_timezone_set('Asia/Jakarta');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['id_invoice'])) {
+        $id_invoice = $_GET['id_invoice'];
+
+        $resultPayment = mysqli_query($conn, "SELECT * FROM tb_payment WHERE id_invoice = '$id_invoice'");
+
+        while ($row = $resultPayment->fetch_array(MYSQLI_ASSOC)) {
+            $paymentArray[] = $row;
+        }
+
+        mysqli_close($conn);
+
+        if ($paymentArray == null) {
+            echo json_encode(array("status" => "empty", "results" => []));
+        } else {
+            echo json_encode(array("status" => "ok", "results" => $paymentArray));
+        }
+    }
+}
