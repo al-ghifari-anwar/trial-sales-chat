277bc6eac84420d625dbc0434300f964e54977f9
Al Ghifari <alghifari.anwar2002@gmail.com>
2023-10-29 23:19:06 +0700

    fix visit

--- visit.php
+++ visit.php
@@ -41,29 +41,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $laporan_visit = $_POST['laporan_visit'];
     $id_user = $_POST['id_user'];
 
-    $cekBid = mysqli_query($conn, "SELECT * FROM tb_bid WHERE id_user = '$id_user' AND id_contact = '$id_contact' AND is_active = 1");
-    $rowBid = $cekBid->fetch_array(MYSQLI_ASSOC);
+    // $cekBid = mysqli_query($conn, "SELECT * FROM tb_bid WHERE id_user = '$id_user' AND id_contact = '$id_contact' AND is_active = 1");
+    // $rowBid = $cekBid->fetch_array(MYSQLI_ASSOC);
 
-    if ($rowBid != null) {
-        $insertVisit = mysqli_query($conn, "INSERT INTO tb_visit(id_contact,distance_visit,laporan_visit,id_user) VALUES($id_contact, $distance_visit, '$laporan_visit', $id_user)");
+    // if ($rowBid != null) {
+    $insertVisit = mysqli_query($conn, "INSERT INTO tb_visit(id_contact,distance_visit,laporan_visit,id_user) VALUES($id_contact, $distance_visit, '$laporan_visit', $id_user)");
 
-        if ($insertVisit) {
-            $id_bid = $rowBid['id_bid'];
-            $insertAction = mysqli_query($conn, "INSERT INTO tb_action_bid(id_bid, field_action_bid) VALUES($id_bid, 'Visit to store')");
+    if ($insertVisit) {
+        $id_bid = $rowBid['id_bid'];
+        // $insertAction = mysqli_query($conn, "INSERT INTO tb_action_bid(id_bid, field_action_bid) VALUES($id_bid, 'Visit to store')");
 
-            if ($insertAction) {
-                $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim laporan!"];
-                echo json_encode($response);
-            } else {
-                $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan record!", "detail" => mysqli_error($conn)];
-                echo json_encode($response);
-            }
-        } else {
-            $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan laporan!", "detail" => mysqli_error($conn)];
-            echo json_encode($response);
-        }
+        // if ($insertAction) {
+        $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim laporan!"];
+        echo json_encode($response);
+        // } else {
+        //     $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan record!", "detail" => mysqli_error($conn)];
+        //     echo json_encode($response);
+        // }
     } else {
-        $response = ["response" => 200, "status" => "failed", "message" => "Tidak bisa mengirim laporan karena belum melakukan bidding!", "detail" => mysqli_error($conn)];
+        $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan laporan!", "detail" => mysqli_error($conn)];
         echo json_encode($response);
     }
+    // } else {
+    //     $response = ["response" => 200, "status" => "failed", "message" => "Tidak bisa mengirim laporan karena belum melakukan bidding!", "detail" => mysqli_error($conn)];
+    //     echo json_encode($response);
+    // }
 }
