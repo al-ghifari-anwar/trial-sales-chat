168891ca8ee76940952c64bb9faffd635db63c93
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2023-12-02 18:25:47 +0700

    List user

--- users.php
+++ users.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             echo json_encode(array("status" => "ok", "results" => $transArray));
         }
     } else {
-        $id_distributor = $_GET['id_distributor'];
+        $id_distributor = $_GET['dst'];
 
         $result = mysqli_query($conn, "SELECT * FROM tb_user JOIN tb_city ON tb_user.id_city = tb_city.id_city WHERE id_distributor = '$id_distributor'");
 
