9d3d8996cc222b1cb80e175d6dba9570ed214888
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2023-12-18 06:33:40 +0700

    Insert voucher

---
+++ insertVoucher.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+include_once("config.php");
+date_default_timezone_set('Asia/Jakarta');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $jmlVoucher = $_GET['j'];
+    $id_contact = $_GET['s'];
+
+    for ($i = 0; $i < $jmlVoucher; $i++) {
+        $no_voucher = rand(00001, 99999);
+
+        $cekVoucher = mysqli_query($conn, "SELECT * FROM tb_voucher WHERE no_voucher = '$no_voucher'");
+        $rowVoucher = $cekVoucher->fetch_array(MYSQLI_ASSOC);
+
+        if (!$rowVoucher) {
+            $insert = mysqli_query($conn, "INSERT INTO tb_voucher(id_contact,no_voucher,point_voucher) VALUES($id_contact,'$no_voucher',1)");
+        }
+    }
+
+    $response = ["response" => 200, "status" => "ok", "message" => $jmlVoucher . " voucher telah berhasil diinput"];
+    echo json_encode($response);
+}
