62e1a5c0c86d44f7a98b55f034f5b444dd6d47e0
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-05 10:41:22 +0700

    Filter by kurir

--- delivery.php
+++ delivery.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     } else if (isset($_GET['id_courier'])) {
         $id_courier = $_GET['id_courier'];
 
-        $result = mysqli_query($conn, "SELECT * FROM tb_delivery JOIN tb_user ON tb_user.id_user = tb_delivery.id_courier WHERE tb_user.id_courier = '$id_courier'");
+        $result = mysqli_query($conn, "SELECT * FROM tb_delivery JOIN tb_user ON tb_user.id_user = tb_delivery.id_courier WHERE tb_delivery.id_courier = '$id_courier'");
 
         while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
             $transArray[] = $row;
