162fd4e70b821d169c6a598d4208b243228cfc4c
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-17 14:33:50 +0700

    awdadawd

--- suratjalanNotClosing.php
+++ suratjalanNotClosing.php
@@ -18,5 +18,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         } else {
             echo json_encode(array("status" => "ok", "results" => $suratJalanArray));
         }
+    } else {
+        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact WHERE tb_surat_jalan.is_closing = 0 ORDER BY id_surat_jalan DESC");
+
+        while ($row = $resultSuratJalan->fetch_array(MYSQLI_ASSOC)) {
+            $suratJalanArray[] = $row;
+        }
+
+        if ($suratJalanArray == null) {
+            echo json_encode(array("status" => "empty", "results" => []));
+        } else {
+            echo json_encode(array("status" => "ok", "results" => $suratJalanArray));
+        }
     }
 }
