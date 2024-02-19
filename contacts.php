f8029c7fe0072ba50123120b3cbdd2bf33b7e31b
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-19 14:11:24 +0700

    payment method

--- contacts.php
+++ contacts.php
@@ -220,7 +220,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $imgNewName = $rowContact['ktp_owner'];
         }
 
-        $result = mysqli_query($conn, "UPDATE tb_contact SET nama = '$nama', tgl_lahir = '$tgl_lahir', store_owner = '$store_owner', id_city = '$id_city', maps_url = '$mapsUrl', address = '$address', nomorhp = '$nomor_hp', termin_payment = $termin_payment, ktp_owner = '$imgNewName', id_promo = '$id_promo', reputation = '$reputation', payment_method = '$payment_method' WHERE id_contact = '$id'");
+        $result = mysqli_query($conn, "UPDATE tb_contact SET nama = '$nama', tgl_lahir = '$tgl_lahir', store_owner = '$store_owner', id_city = '$id_city', maps_url = '$mapsUrl', address = '$address', nomorhp = '$nomor_hp', termin_payment = $termin_payment, ktp_owner = '$imgNewName', id_promo = '$id_promo', reputation = '$reputation', payment_method = '$payment_method', store_status = '$status' WHERE id_contact = '$id'");
 
         if ($result) {
             $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengubah data kontak!"];
