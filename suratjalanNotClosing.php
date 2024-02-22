aa82ecf8a3b975e5eb89af5fd48d272bcd9c6b72
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-22 07:43:27 +0700

    SJ not closing by dst and city

--- suratjalanNotClosing.php
+++ suratjalanNotClosing.php
@@ -4,21 +4,21 @@ include_once("config.php");
 date_default_timezone_set('Asia/Jakarta');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($_GET['c'])) {
-        $id_city = $_GET['c'];
+    // if (isset($_GET['c'])) {
+    //     $id_city = $_GET['c'];
 
-        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact WHERE tb_contact.id_city = '$id_city' AND tb_surat_jalan.is_closing = 0 ORDER BY id_surat_jalan DESC");
+    //     $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact WHERE tb_contact.id_city = '$id_city' AND tb_surat_jalan.is_closing = 0 ORDER BY id_surat_jalan DESC");
 
-        while ($row = $resultSuratJalan->fetch_array(MYSQLI_ASSOC)) {
-            $suratJalanArray[] = $row;
-        }
+    //     while ($row = $resultSuratJalan->fetch_array(MYSQLI_ASSOC)) {
+    //         $suratJalanArray[] = $row;
+    //     }
 
-        if ($suratJalanArray == null) {
-            echo json_encode(array("status" => "empty", "results" => []));
-        } else {
-            echo json_encode(array("status" => "ok", "results" => $suratJalanArray));
-        }
-    }
+    //     if ($suratJalanArray == null) {
+    //         echo json_encode(array("status" => "empty", "results" => []));
+    //     } else {
+    //         echo json_encode(array("status" => "ok", "results" => $suratJalanArray));
+    //     }
+    // }
     if (isset($_GET['dst']) && isset($_GET['c'])) {
         $id_distributor = $_GET['dst'];
         $id_city = $_GET['c'];
