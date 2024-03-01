63baaf28f8f29067b0c132829fe58ce7ae1b9175
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-03-01 16:08:50 +0700

    Data kurir SJ not closing

--- suratjalanNotClosing.php
+++ suratjalanNotClosing.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $id_distributor = $_GET['dst'];
         $id_city = $_GET['c'];
 
-        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact JOIN tb_city ON tb_contact.id_city = tb_city.id_city WHERE tb_city.id_distributor = '$id_distributor' AND tb_surat_jalan.is_closing = 0 AND tb_city.id_city = '$id_city' ORDER BY id_surat_jalan DESC");
+        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact JOIN tb_city ON tb_contact.id_city = tb_city.id_city JOIN tb_user ON tb_user.id_user = tb_surat_jalan.id_courier WHERE tb_city.id_distributor = '$id_distributor' AND tb_surat_jalan.is_closing = 0 AND tb_city.id_city = '$id_city' ORDER BY id_surat_jalan DESC");
 
         while ($row = $resultSuratJalan->fetch_array(MYSQLI_ASSOC)) {
             $suratJalanArray[] = $row;
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $id_distributor = $_GET['dst'];
         $id_city = $_GET['c'];
 
-        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact JOIN tb_city ON tb_contact.id_city = tb_city.id_city WHERE tb_city.id_distributor = '$id_distributor' AND tb_surat_jalan.is_closing = 0 ORDER BY id_surat_jalan DESC");
+        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact JOIN tb_city ON tb_contact.id_city = tb_city.id_city JOIN tb_user ON tb_user.id_user = tb_surat_jalan.id_courier WHERE tb_city.id_distributor = '$id_distributor' AND tb_surat_jalan.is_closing = 0 ORDER BY id_surat_jalan DESC");
 
         while ($row = $resultSuratJalan->fetch_array(MYSQLI_ASSOC)) {
             $suratJalanArray[] = $row;
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             echo json_encode(array("status" => "ok", "results" => $suratJalanArray));
         }
     } else {
-        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact WHERE tb_surat_jalan.is_closing = 0 ORDER BY id_surat_jalan DESC");
+        $resultSuratJalan = mysqli_query($conn, "SELECT * FROM tb_surat_jalan JOIN tb_contact ON tb_contact.id_contact = tb_surat_jalan.id_contact JOIN tb_user ON tb_user.id_user = tb_surat_jalan.id_courier WHERE tb_surat_jalan.is_closing = 0 ORDER BY id_surat_jalan DESC");
 
         while ($row = $resultSuratJalan->fetch_array(MYSQLI_ASSOC)) {
             $suratJalanArray[] = $row;
