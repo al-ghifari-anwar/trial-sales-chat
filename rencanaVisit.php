4b7fb247098af7d7bdd094111b519d0478ca4506
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-06-27 09:16:13 +0700

    marketing konten

--- rencanaVisit.php
+++ rencanaVisit.php
@@ -80,7 +80,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     } else if ($_GET['type'] == 'passive') {
         if (isset($_GET['c'])) {
             $id_city = $_GET['c'];
-            $getRenvis = mysqli_query($conn, "SELECT tb_rencana_visit.*, tb_contact.nama, tb_contact.nomorhp, tb_contact.id_city, tb_contact.store_status, tb_contact.store_owner, tb_contact.maps_url, tb_contact.created_at AS created_at_store, tb_contact.reputation, tb_contact.id_contact FROM tb_rencana_visit JOIN tb_contact ON tb_contact.id_contact = tb_rencana_visit.id_contact WHERE type_rencana = 'passive' AND tb_contact.id_city = '$id_city' AND is_visited = 0");
+            $getRenvis = mysqli_query($conn, "SELECT tb_rencana_visit.*, tb_contact.nama, tb_contact.nomorhp, tb_contact.id_city, tb_contact.store_status, tb_contact.store_owner, tb_contact.maps_url, tb_contact.created_at AS created_at_store, tb_contact.reputation, tb_contact.id_contact FROM tb_rencana_visit JOIN tb_contact ON tb_contact.id_contact = tb_rencana_visit.id_contact WHERE type_rencana = 'passive' AND tb_contact.id_city = '$id_city' AND is_visited = 0 GROUP BY tb_rencana_visit.id_contact");
 
             while ($rowRenvis = $getRenvis->fetch_array(MYSQLI_ASSOC)) {
                 $id_con = $rowRenvis['id_contact'];
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             }
         } else {
             $id_distributor = $_GET['dst'];
-            $getRenvis = mysqli_query($conn, "SELECT tb_rencana_visit.*, tb_contact.nama, tb_contact.nomorhp, tb_contact.id_city, tb_contact.store_status, tb_city.*, tb_contact.store_owner, tb_contact.maps_url, tb_contact.created_at AS created_at_store, tb_contact.reputation, tb_contact.id_contact FROM tb_rencana_visit JOIN tb_contact ON tb_contact.id_contact = tb_rencana_visit.id_contact JOIN tb_city ON tb_city.id_city = tb_contact.id_city WHERE type_rencana = 'passive' AND tb_city.id_distributor = '$id_distributor' AND is_visited = 0");
+            $getRenvis = mysqli_query($conn, "SELECT tb_rencana_visit.*, tb_contact.nama, tb_contact.nomorhp, tb_contact.id_city, tb_contact.store_status, tb_city.*, tb_contact.store_owner, tb_contact.maps_url, tb_contact.created_at AS created_at_store, tb_contact.reputation, tb_contact.id_contact FROM tb_rencana_visit JOIN tb_contact ON tb_contact.id_contact = tb_rencana_visit.id_contact JOIN tb_city ON tb_city.id_city = tb_contact.id_city WHERE type_rencana = 'passive' AND tb_city.id_distributor = '$id_distributor' AND is_visited = 0 GROUP BY tb_rencana_visit.id_contact");
 
             while ($rowRenvis = $getRenvis->fetch_array(MYSQLI_ASSOC)) {
                 $id_con = $rowRenvis['id_contact'];
