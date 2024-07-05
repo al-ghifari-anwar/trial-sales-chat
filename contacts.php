9323c0aa740c1ff1c401552d54f6b6065c0b9d3c
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-05 13:53:42 +0700

    2 nomor

--- contacts.php
+++ contacts.php
@@ -223,7 +223,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         }
 
         $result = mysqli_query($conn, "UPDATE tb_contact SET nama = '$nama', tgl_lahir = '$tgl_lahir', store_owner = '$store_owner', id_city = '$id_city', maps_url = '$mapsUrl', address = '$address', nomorhp = '$nomor_hp', termin_payment = $termin_payment, ktp_owner = '$imgNewName', id_promo = '$id_promo', reputation = '$reputation', payment_method = '$payment_method', store_status = '$status', tagih_mingguan = $tagih_mingguan, nomorhp_2 = '$nomor_hp2' WHERE id_contact = '$id'");
-
+        // 
         if ($result) {
             $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengubah data kontak!"];
             echo json_encode($response);
