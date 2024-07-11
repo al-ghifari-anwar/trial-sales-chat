7332185c4e7288a42d9c1f2cd5bc57a8a45fdeca
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-12 05:24:18 +0700

    remove renvi voucher

---
+++ autoRemoveRenvisVoucher.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+include_once("config.php");
+date_default_timezone_set('Asia/Jakarta');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $getMaxVoucher = mysqli_query($conn, "SELECT MAX(id_voucher) as id_voucher FROM tb_voucher GROUP BY id_contact");
+    while ($rowMaxVoucher = $getMaxVoucher->fetch_array(MYSQLI_ASSOC)) {
+        $arrMaxVouhcer[] = $rowMaxVoucher;
+    }
+
+    foreach ($arrMaxVouhcer as $maxVoucher) {
+        $id_voucher = $maxVoucher['id_voucher'];
+        $getVouhcer = mysqli_query($conn, "SELECT * FROM tb_voucher WHERE id_voucher = '$id_voucher'");
+        $rowVoucher = $getVouhcer->fetch_array(MYSQLI_ASSOC);
+
+        $id_contact = $rowVoucher['id_contact'];
+
+        $getContact = mysqli_query($conn, "SELECT * FROM tb_contact WHERE id_contact = '$id_contact'");
+        $rowContact = $getContact->fetch_array(MYSQLI_ASSOC);
+
+        if (date("Y-m-d", strtotime($rowVoucher['exp_date'])) < date("Y-m-d")) {
+            $response = ["response" => 200, "status" => "ok", "message" => "Vouhcer telah expired!", "detail" => "ID:" . $id_contact . "| NAME:" . $rowContact['nama']];
+            echo json_encode($response);
+        } else {
+            if ($rowVoucher['is_claimed'] == 1) {
+                $response = ["response" => 200, "status" => "ok", "message" => "Vouhcer telah claim!", "detail" => "ID:" . $id_contact . "| NAME:" . $rowContact['nama']];
+                echo json_encode($response);
+            } else {
+                $response = ["response" => 200, "status" => "ok", "message" => "Vouhcer masih renvi!"];
+                echo json_encode($response);
+            }
+        }
+    }
+}
