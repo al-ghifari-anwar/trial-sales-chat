a552e313923542ae0c421f8cecc6278205691f4f
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-15 11:48:25 +0700

    antrian remove interval

--- antrianRenvis.php
+++ antrianRenvis.php
@@ -16,48 +16,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $date_renvis = $antrianArray['date_renvis'];
         $interval_renvis = $antrianArray['interval_renvis'];
 
-        if ($date_renvis == date("Y-m-d")) {
-            $cekRenvis = mysqli_query($conn, "SELECT * FROM tb_rencana_visit WHERE id_contact = '$id_contact' AND type_rencana = 'passive' AND is_visited = 0");
+        // if ($date_renvis == date("Y-m-d")) {
+        $cekRenvis = mysqli_query($conn, "SELECT * FROM tb_rencana_visit WHERE id_contact = '$id_contact' AND type_rencana = 'passive' AND is_visited = 0");
 
-            // while ($rowRenvis = $cekRenvis->fetch_array(MYSQLI_ASSOC)) {
-            $renvisArray = $cekRenvis->fetch_array(MYSQLI_ASSOC);
-            // }
+        // while ($rowRenvis = $cekRenvis->fetch_array(MYSQLI_ASSOC)) {
+        $renvisArray = $cekRenvis->fetch_array(MYSQLI_ASSOC);
+        // }
 
-            if ($renvisArray == null) {
-                $insertRenvis = mysqli_query($conn, "INSERT INTO tb_rencana_visit(id_contact,id_surat_jalan,type_rencana,id_distributor,id_invoice) VALUES($id_contact,0,'passive',$id_distributor,0)");
+        if ($renvisArray == null) {
+            $insertRenvis = mysqli_query($conn, "INSERT INTO tb_rencana_visit(id_contact,id_surat_jalan,type_rencana,id_distributor,id_invoice) VALUES($id_contact,0,'passive',$id_distributor,0)");
 
-                if ($insertRenvis) {
-                    $response = ["response" => 200, "status" => "ok", "message" => "Berhasil menyimpan data rencana visit!"];
-                    echo json_encode($response);
-                } else {
-                    $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan data rencana visit!", "detail" => mysqli_error($conn)];
-                    echo json_encode($response);
-                }
+            if ($insertRenvis) {
+                $response = ["response" => 200, "status" => "ok", "message" => "Berhasil menyimpan data rencana visit!"];
+                echo json_encode($response);
             } else {
-                $response = ["message" => "Sudah ada"];
+                $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan data rencana visit!", "detail" => mysqli_error($conn)];
                 echo json_encode($response);
             }
         } else {
-            $cekRenvis = mysqli_query($conn, "SELECT COUNT(*) AS jml_renvis FROM tb_rencana_visit WHERE id_contact = '$id_contact' AND type_rencana = 'passive'");
-            $renvisArray = $cekRenvis->fetch_array(MYSQLI_ASSOC);
-            $jmlRenvis = $renvisArray['jml_renvis'];
-
-            $times = $jmlRenvis * $interval_renvis;
-
-            if ($date_renvis == date("Y-m-d", strtotime("-" . $times . " days"))) {
-                $insertRenvis = mysqli_query($conn, "INSERT INTO tb_rencana_visit(id_contact,id_surat_jalan,type_rencana,id_distributor,id_invoice) VALUES($id_contact,0,'passive',$id_distributor,0)");
-
-                if ($insertRenvis) {
-                    $response = ["response" => 200, "status" => "ok", "message" => "Berhasil menyimpan data rencana visit!"];
-                    echo json_encode($response);
-                } else {
-                    $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan data rencana visit!", "detail" => mysqli_error($conn)];
-                    echo json_encode($response);
-                }
-            } else {
-                $response = ["message" => "Belum waktunya interval", "detail" => "JML: " . $jmlRenvis . "| INTERVAL:" . $interval_renvis . "| DATE:" . $date_renvis . "| DATE MINUS:" . date("Y-m-d", strtotime("-" . $times . " days")) . "| ID: " . $id_contact];
-                echo json_encode($response);
-            }
+            $response = ["message" => "Sudah ada"];
+            echo json_encode($response);
         }
+        // } else {
+        //     $cekRenvis = mysqli_query($conn, "SELECT COUNT(*) AS jml_renvis FROM tb_rencana_visit WHERE id_contact = '$id_contact' AND type_rencana = 'passive'");
+        //     $renvisArray = $cekRenvis->fetch_array(MYSQLI_ASSOC);
+        //     $jmlRenvis = $renvisArray['jml_renvis'];
+
+        //     $times = $jmlRenvis * $interval_renvis;
+
+        //     if ($date_renvis == date("Y-m-d", strtotime("-" . $times . " days"))) {
+        //         $insertRenvis = mysqli_query($conn, "INSERT INTO tb_rencana_visit(id_contact,id_surat_jalan,type_rencana,id_distributor,id_invoice) VALUES($id_contact,0,'passive',$id_distributor,0)");
+
+        //         if ($insertRenvis) {
+        //             $response = ["response" => 200, "status" => "ok", "message" => "Berhasil menyimpan data rencana visit!"];
+        //             echo json_encode($response);
+        //         } else {
+        //             $response = ["response" => 200, "status" => "failed", "message" => "Gagal menyimpan data rencana visit!", "detail" => mysqli_error($conn)];
+        //             echo json_encode($response);
+        //         }
+        //     } else {
+        //         $response = ["message" => "Belum waktunya interval", "detail" => "JML: " . $jmlRenvis . "| INTERVAL:" . $interval_renvis . "| DATE:" . $date_renvis . "| DATE MINUS:" . date("Y-m-d", strtotime("-" . $times . " days")) . "| ID: " . $id_contact];
+        //         echo json_encode($response);
+        //     }
+        // }
     }
 }
