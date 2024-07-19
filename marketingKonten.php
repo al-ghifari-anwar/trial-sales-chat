df4cfb1140d997b5cb8a1058cf18a969204510ab
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-19 15:11:21 +0700

    test marketing konten

--- marketingKonten.php
+++ marketingKonten.php
@@ -6,18 +6,29 @@ date_default_timezone_set('Asia/Jakarta');
 $wa_token = '_GEJodr1x8u7-nSn4tZK2hNq0M5CARkRp_plNdL2tFw';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $getMarketingMsg = mysqli_query($conn, "SELECT * FROM tb_marketing_message");
+    $getStore = mysqli_query($conn, "SELECT * FROM tb_contact JOIN id_city ON tb_city.id_city = tb_contact.id_city WHERE store_status != 'blacklist'");
 
-    while ($rowMarketingMsg = $getMarketingMsg->fetch_array(MYSQLI_ASSOC)) {
-        $marketingMsgArr[] = $rowMarketingMsg;
+    while ($rowStore = $getStore->fetch_array(MYSQLI_ASSOC)) {
+        $storeArray[] = $rowStore;
     }
 
-    // echo json_encode($marketingMsgArr);
-    // die;
+    foreach ($storeArray as $store) {
+        $nomor_hp = $store['nomorhp'];
+        $nama = $store['nama'];
+        $id_distributor = $store['id_distributor'];
+        $created_at = $store['created_at'];
 
-    if ($marketingMsgArr != null) {
+        $getQontak = mysqli_query($conn, "SELECT * FROM tb_qontak WHERE id_distributor = '$id_distributor'");
+        $rowQontak = $getQontak->fetch_array(MYSQLI_ASSOC);
 
-        foreach ($marketingMsgArr as $marketingMsg) {
+        $integration_id = $rowQontak['integration_id'];
+
+        $getMarketingKonten = mysqli_query($conn, "SELECT * FROM tb_marketing_message WHERE id_distributor = '$id_distributor'");
+        while ($rowMarketingKonten = $getMarketingKonten->fetch_array(MYSQLI_ASSOC)) {
+            $marketingKontenArray[] = $rowMarketingKonten;
+        }
+
+        foreach ($marketingKontenArray as $marketingKonten) {
             $template_id = $marketingMsg['template_id'];
             $image = "https://order.topmortarindonesia.com/assets/img/content_img/" . $marketingMsg['image_marketing_message'];
             $body = $marketingMsg['body_marketing_message'];
@@ -25,57 +36,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $target_status = $marketingMsg['target_status'];
             $id_distributor = $marketingMsg['id_distributor'];
 
-            $getStore = mysqli_query($conn, "SELECT * FROM tb_contact JOIN tb_city ON tb_city.id_city = tb_contact.id_city WHERE tb_city.id_distributor = '$id_distributor'");
+            // if ($week == 1) {
+            $dateMinusWeek = date("Y-m-d", strtotime("-" . $week . " days"));
+            // } else if ($week > 2) {
+            //     $dateMinusWeek = date("Y-m-d", strtotime("-" . $week . " day"));
+            // } else if ($week == 0) {
+            //     $dateMinusWeek = date("Y-m-d");
+            // }
+            if ($dateMinusWeek == $created_at) {
+                // Send Message
+                $curl = curl_init();
 
-            $qontak = null;
-            $getQontak = mysqli_query($conn, "SELECT * FROM tb_qontak WHERE id_distributor = '$id_distributor'");
-            $qontak = $getQontak->fetch_array(MYSQLI_ASSOC);
-
-            $integration_id = $qontak['integration_id'];
-
-            // $storeArr = array();
-            while ($rowStore = $getStore->fetch_array(MYSQLI_ASSOC)) {
-                $storeArr[] = $rowStore;
-            }
-
-            if ($week == 1) {
-                $dateMinusWeek = date("Y-m-d", strtotime("-" . $week . " day"));
-            } else if ($week > 2) {
-                $dateMinusWeek = date("Y-m-d", strtotime("-" . $week . " day"));
-            } else if ($week == 0) {
-                $dateMinusWeek = date("Y-m-d");
-            }
-
-            // $response = ["response" => 200, "status" => "failed", "contacts" => $storeArr, "dst" => $id_distributor];
-            // echo json_encode($response);
-            // die;
-
-            if ($storeArr != null) {
-
-                foreach ($storeArr as $store) {
-                    $created_at = date("Y-m-d", strtotime($store['created_at']));
-                    // $dateMinusWeek = date("Y-m-d");
-
-                    $nama = $store['nama'];
-                    $nomor_hp = $store['nomorhp'];
-                    $id_contact = $store['id_contact'];
-
-                    if ($dateMinusWeek == $created_at) {
-                        // if ($id_contact == 1670) {
-
-                        // Send Message
-                        $curl = curl_init();
-
-                        curl_setopt_array($curl, array(
-                            CURLOPT_URL => 'https://service-chat.qontak.com/api/open/v1/broadcasts/whatsapp/direct',
-                            CURLOPT_RETURNTRANSFER => true,
-                            CURLOPT_ENCODING => '',
-                            CURLOPT_MAXREDIRS => 10,
-                            CURLOPT_TIMEOUT => 0,
-                            CURLOPT_FOLLOWLOCATION => true,
-                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                            CURLOPT_CUSTOMREQUEST => 'POST',
-                            CURLOPT_POSTFIELDS => '{
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://service-chat.qontak.com/api/open/v1/broadcasts/whatsapp/direct',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => '{
                                 "to_number": "' . $nomor_hp . '",
                                 "to_name": "' . $nama . '",
                                 "message_template_id": "' . $template_id . '",
@@ -106,44 +87,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                                     ]
                                 }
                                 }',
-                            CURLOPT_HTTPHEADER => array(
-                                'Authorization: Bearer ' . $wa_token,
-                                'Content-Type: application/json'
-                            ),
-                        ));
+                    CURLOPT_HTTPHEADER => array(
+                        'Authorization: Bearer ' . $wa_token,
+                        'Content-Type: application/json'
+                    ),
+                ));
 
-                        $response = curl_exec($curl);
+                $response = curl_exec($curl);
 
-                        curl_close($curl);
+                curl_close($curl);
 
-                        $res = json_decode($response, true);
+                $res = json_decode($response, true);
 
-                        $status = $res['status'];
+                $status = $res['status'];
 
-                        if ($status == "success") {
-                            $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim konten marketing!", "details" => 'Contact:' . $id_contact . "|Nama:" . $nama . "|" . $nomor_hp . "| DateMinus: " . $dateMinusWeek . "| CreatedAt: " . $created_at];
-                            echo json_encode($response);
-                        } else {
-                            // $response = ["response" => 200, "status" => "failed", "message" => "Gagal mengirim konten marketing!"];
-                            // echo json_encode($response);
-                            echo $response;
-                        }
-                        // } else {
-                        //     $response = ["response" => 200, "status" => "failed", "message" => "Not for testing!"];
-                        //     echo json_encode($response);
-                        // }
-                    } else {
-                        $response = ["response" => 200, "status" => "failed", "message" => "Tidak kirim konten karna bukan waktunya!", "details" => 'Contact:' . $id_contact . "|Nama:" . $nama . "|" . $nomor_hp . "| DateMinus: " . $dateMinusWeek . "| CreatedAt: " . $created_at . "| week:" . $week];
-                        echo json_encode($response);
-                    }
+                if ($status == "success") {
+                    $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim konten marketing!", "details" => 'Contact:' . $id_contact . "|Nama:" . $nama . "|" . $nomor_hp . "| DateMinus: " . $dateMinusWeek . "| CreatedAt: " . $created_at];
+                    echo json_encode($response);
+                } else {
+                    // $response = ["response" => 200, "status" => "failed", "message" => "Gagal mengirim konten marketing!"];
+                    // echo json_encode($response);
+                    echo $response;
                 }
             } else {
-                $response = ["response" => 200, "status" => "failed", "message" => "Store is null!"];
+                $response = ["response" => 200, "status" => "failed", "message" => "Tidak kirim konten karna bukan waktunya!", "details" => 'Contact:' . $id_contact . "|Nama:" . $nama . "|" . $nomor_hp . "| DateMinus: " . $dateMinusWeek . "| CreatedAt: " . $created_at . "| week:" . $week];
                 echo json_encode($response);
             }
         }
-    } else {
-        $response = ["response" => 200, "status" => "failed", "message" => "Konten tidak ditemukan!"];
-        echo json_encode($response);
     }
 }
