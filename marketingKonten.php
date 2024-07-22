e1e2024f7139d7985e3835b05ceec27c060b92ac
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-22 17:56:48 +0700

    test marketing konten

--- marketingKonten.php
+++ marketingKonten.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     // echo json_encode($store);
                     $id_contact = $store['id_contact'];
                     $nomor_hp = $store['nomorhp'];
-                    // $nama = $store['nama'];
+                    $nama = $store['nama'];
                     // $created_at = $store['created_at'];
                     // Send Message
                     // $curl = curl_init();
