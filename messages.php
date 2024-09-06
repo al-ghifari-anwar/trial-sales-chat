6f349f55e2aa4b36d7cd3167846b547443cdd484
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-09-07 05:26:04 +0700

    qontak admin tkp

--- messages.php
+++ messages.php
@@ -105,7 +105,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $integration_id = $rowUserData['integration_id'];
             $template_id = 'c64f15db-c5f0-4d9b-9888-4b24624d9e5c';
             if ($rowUserData['level_user'] == 'admin') {
-                $integration_id = "EGzGoRR6sw6B5FhpJsG_Y2HB8g9f1U6amBOC9VJHITY";
+                $wa_token = "EGzGoRR6sw6B5FhpJsG_Y2HB8g9f1U6amBOC9VJHITY";
+                $integration_id = "38654c8b-76a1-45d9-a5ae-969e4bf3fb83";
                 $template_id = '85f17083-255d-4340-af32-5dd22f483960';
             }
         }
