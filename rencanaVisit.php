f51fb04632a2825c474e63e18e9e1fb0e2920c23
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-09-24 09:00:03 +0700

    Edit data tukang disabled

--- rencanaVisit.php
+++ rencanaVisit.php
@@ -99,7 +99,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 $count = mysqli_query($conn, "SELECT COUNT(*) AS jmlRenvis FROM tb_rencana_visit WHERE id_contact = '$id_con' AND type_rencana = 'passive'");
                 $resCount = $count->fetch_array(MYSQLI_ASSOC);
                 $date_margin = date("Y-m-d", strtotime("-1 month"));
-                $lastVisit = mysqli_query($conn, "SELECT * FROM tb_visit WHERE id_contact = '$id_con' AND source_visit IN ('jatem1','jatem2','jatem3','weekly','voucher','passive') AND date_visit >= '$date_margin' ORDER BY date_visit DESC LIMIT 1");
+                $lastVisit = mysqli_query($conn, "SELECT * FROM tb_visit WHERE id_contact = '$id_con' AND source_visit IN ('jatem1','jatem2','jatem3','weekly','voucher','passive','renvisales') AND date_visit >= '$date_margin' ORDER BY date_visit DESC LIMIT 1");
                 $resLastVisit = $lastVisit->fetch_array(MYSQLI_ASSOC);
                 $rowRenvis['last_visit'] = $resLastVisit == null ? '0000-00-00' : $resLastVisit['date_visit'];
                 $created_at = $rowRenvis['created_at'];
