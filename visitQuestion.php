d991fbb8cf0c42df240a0b527c551c5e66551c83
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-18 15:31:22 +0700

    Send answer

--- visitQuestion.php
+++ visitQuestion.php
@@ -23,8 +23,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id_visit = $_POST['id_visit'];
     $answers = $_POST['array_answer'];
-    $decode_answers = html_entity_decode($answers);
-    $answers = json_decode($decode_answers, true);
+    // $decode_answers = html_entity_decode($answers);
+    $answers = json_decode($answers, true);
+    $answers = json_decode($answers, true);
     // $answers = json_encode($answers);
 
     // echo $answers;
