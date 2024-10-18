9176b5465e686252d5327c4e6b86e6c3b70131f8
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-18 15:18:13 +0700

    Send answer

--- visitQuestion.php
+++ visitQuestion.php
@@ -30,8 +30,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     foreach ($answers as $answer) {
         $id_question = $answer['id_visit_question'];
-        // echo $answer;
-        print_r($answer);
+        echo json_encode($answer);
+        // print_r($answer);
         die;
 
         $getQuestion = mysqli_query($conn, "SELECT * FROM tb_visit_question WHERE id_visit_question = '$id_question'");
