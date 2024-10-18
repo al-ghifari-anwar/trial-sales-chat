edd2020ff563646712f8807122d6d773ebd17769
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-18 15:22:31 +0700

    Send answer

--- visitQuestion.php
+++ visitQuestion.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $answers = $_POST['array_answer'];
     $answers = json_decode($answers, true);
 
-    echo $answers[2];
+    echo $answers['id_visit_question'];
     die;
 
     foreach ($answers as $answer) {
