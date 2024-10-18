9e7ad148efdd2efd2af69f421010b1bbf264b55c
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-18 14:42:46 +0700

    Send answer

--- visitQuestion.php
+++ visitQuestion.php
@@ -21,24 +21,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         echo json_encode(array("status" => "ok", "results" => $arrayQuestion));
     }
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id_question = $_POST['id_question'];
-    $text_answer = $_POST['text_answer'];
+    $id_visit = $_POST['id_visit'];
+    $answers = $_POST['array_answer'];
+    $answers = json_decode($answers, true);
 
-    $getQuestion = mysqli_query($conn, "SELECT * FROM tb_visit_question WHERE id_question = '$id_question'");
-    $rowQuestion = $getQuestion->fetch_array(MYSQLI_ASSOC);
+    foreach ($answers as $answer) {
+        $id_question = $answer['id_visit_question'];
 
-    $answer_type = $rowQuestion['answer_type'];
-    $text_question = $rowQuestion['text_question'];
+        $getQuestion = mysqli_query($conn, "SELECT * FROM tb_visit_question WHERE id_question = '$id_question'");
+        $rowQuestion = $getQuestion->fetch_array(MYSQLI_ASSOC);
 
-    if ($answer_type == 'text' || $answer_type == 'radio' || $answer_type == 'date') {
-        $insertAnswer = mysqli_query($conn, "INSERT INTO tb_visit_answer(id_question,text_question,text_answer) VALUES($id_question,'$text_question','$text_answer')");
+        $answer_type = $rowQuestion['answer_type'];
+        $text_question = $rowQuestion['text_question'];
 
-        if ($insertAnswer) {
-            $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim jawaban!"];
-            echo json_encode($response);
-        } else {
-            $response = ["response" => 200, "status" => "failed", "message" => "Gagal mengirim jawaban!"];
-            echo json_encode($response);
+        if ($answer_type == 'text' || $answer_type == 'radio' || $answer_type == 'date') {
+            $text_answer = $answer['text_answer'];
+
+            $insertAnswer = mysqli_query($conn, "INSERT INTO tb_visit_answer(id_question,text_question,text_answer) VALUES($id_question,'$text_question','$text_answer')");
+
+            // if ($insertAnswer) {
+            //     $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim jawaban!"];
+            //     echo json_encode($response);
+            // } else {
+            //     $response = ["response" => 200, "status" => "failed", "message" => "Gagal mengirim jawaban!"];
+            //     echo json_encode($response);
+            // }
+        } else if ($answer_type == 'checkbox') {
+            $selected_answers = $answer['selected_answer'];
+
+            foreach ($selected_answers as $selected_answer) {
+
+                $insertAnswer = mysqli_query($conn, "INSERT INTO tb_visit_answer(id_question,text_question,text_answer) VALUES($id_question,'$text_question','$selected_answer')");
+            }
         }
     }
+
+    $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim jawaban!"];
+    echo json_encode($response);
 }
