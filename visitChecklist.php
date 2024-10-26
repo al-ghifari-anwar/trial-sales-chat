28c1079794c793b5b00c7cf908bee4157c1df00e
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-26 13:18:48 +0700

    Get checklist visit

--- visitChecklist.php
+++ visitChecklist.php
@@ -12,10 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
         // echo mysqli_error($conn);
 
-        $answerArray = array();
         while ($rowVisitAnswer = $getVisitAnswer->fetch_array(MYSQLI_ASSOC)) {
             $text_question = $rowVisitAnswer['text_question'];
             $getAnswer = mysqli_query($conn, "SELECT * FROM tb_visit_answer WHERE id_visit = '$id_visit' AND text_question = '$text_question'");
+            $answerArray = array();
             while ($rowAnswer = $getAnswer->fetch_array(MYSQLI_ASSOC)) {
                 $answerArray[] = $rowAnswer['text_answer'];
             }
