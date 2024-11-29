64ead99ad47581c208e5e283961ffd051908022f
M Rafli Ramadani <keerouk.ink@gmail.com>
2024-11-29 17:02:25 +0700

    fiix change column name

--- clickupWebhook.php
+++ clickupWebhook.php
@@ -14,20 +14,53 @@ header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-    if (isset($_GET['id'])) {
+    if (isset($_GET['cw_id'])) {
 
-        $id = $_GET['id'];
+        $id = $_GET['cw_id'];
 
-        $result = mysqli_query($conn, "SELECT * FROM tb_clickup_webhook WHERE id = '$id'");
+        $result = mysqli_query($conn, "SELECT * FROM tb_clickup_webhook WHERE cw_id = '$id'");
 
         if ($result) {
 
             if (mysqli_num_rows($result) > 0) {
 
                 while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-                    $jsonString = $row['webhook'];
+                    $jsonString = $row['cw_data'];
                     $jsonObject = json_decode($jsonString, true);
-                    $row['webhook'] = json_decode($jsonObject, true);
+                    $row['cw_data'] = json_decode($jsonObject, true);
+                    $transArray[] = $row;
+                }
+
+                if (json_last_error() === JSON_ERROR_NONE) {
+                    echo json_encode(array("status" => "ok", "results" => $transArray));
+                } else {
+                    echo json_encode(array("status" => "empty", "message" => "Error decoding JSON: " . json_last_error_msg(), "results" => []));;
+                }
+
+            } else {
+                echo json_encode(array("status" => "empty", "results" => []));
+            }
+
+        } else {
+            echo json_encode(array("status" => "failed", "error" => mysqli_error($conn)));
+        }
+        
+        mysqli_close($conn);
+
+    } else if (isset($_GET['cw_event'])) {
+
+        $event = $_GET['cw_event'];
+
+        $result = mysqli_query($conn, "SELECT * FROM tb_clickup_webhook WHERE cw_event = '$event'");
+
+        if ($result) {
+
+            if (mysqli_num_rows($result) > 0) {
+
+                while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                    $jsonString = $row['cw_data'];
+                    $jsonObject = json_decode($jsonString, true);
+                    $row['cw_data'] = json_decode($jsonObject, true);
                     $transArray[] = $row;
                 }
 
@@ -56,9 +89,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             if (mysqli_num_rows($result) > 0) {
 
                 while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-                    $jsonString = $row['webhook'];
+                    $jsonString = $row['cw_data'];
                     $jsonObject = json_decode($jsonString, true);
-                    $row['webhook'] = json_decode($jsonObject, true);
+                    $row['cw_data'] = json_decode($jsonObject, true);
                     $transArray[] = $row;
                 }
 
@@ -90,7 +123,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $webhookData = mysqli_real_escape_string($conn, json_encode($input));
         $jsonString = json_encode($webhookData, JSON_PRETTY_PRINT);
 
-        $query = "INSERT INTO tb_clickup_webhook (webhook, event) VALUES ('$jsonString', '$event')";
+        $query = "INSERT INTO tb_clickup_webhook (cw_data, cw_event) VALUES ('$jsonString', '$event')";
 
         if (mysqli_query($conn, $query)) {
             echo json_encode(array("status" => "ok", "message" => "Data stored successfully"));
