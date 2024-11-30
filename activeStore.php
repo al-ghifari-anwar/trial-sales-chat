fde996f64eb9a77036a49ebfd3e4e2d3fb511c5f
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-11-30 08:46:01 +0700

    toko aktif akhir bulan

--- activeStore.php
+++ activeStore.php
@@ -4,15 +4,18 @@ include_once("config.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['mo'])) {
-        $month = $_GET['mo'];
+        // $month = $_GET['mo'];
+        $year = date("Y");
 
-        $resultActive = mysqli_query($conn, "SELECT COUNT(*) AS jml_active FROM tb_status_change WHERE status_from = 'passive' AND status_to = 'active' AND MONTH(created_at) = '$month'");
-        $rowActive = $resultActive->fetch_array(MYSQLI_ASSOC);
+        $resultActive = mysqli_query($conn, "SELECT COUNT(*) AS jml_active FROM tb_status_change WHERE status_from = 'passive' AND status_to = 'active' AND YEAR(created_at) = '$year' GROUP BY MONTH(created_at)");
+        while ($rowActive = $resultActive->fetch_array(MYSQLI_ASSOC)) {
+            $arrayActive[] = $rowActive;
+        }
 
-        if ($rowActive == null) {
+        if ($arrayActive == null) {
             echo json_encode(array("status" => "failed", "results" => []));
         } else {
-            echo json_encode(array("status" => "ok", "results" => $rowActive));
+            echo json_encode(array("status" => "ok", "results" => $arrayActive));
         }
     } else {
         $resultActive = mysqli_query($conn, "SELECT COUNT(*) AS jml_active FROM tb_status_change WHERE status_from = 'passive' AND status_to = 'active'");
