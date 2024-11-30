69e2830e451eb82b8c7d2868600305e6be79b701
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-11-30 09:09:19 +0700

    toko aktif akhir bulan

--- activeStore.php
+++ activeStore.php
@@ -10,7 +10,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     while ($rowActive = $resultActive->fetch_array(MYSQLI_ASSOC)) {
         $month = $rowActive['month_active'];
         $countActive = mysqli_query($conn, "SELECT COUNT(*) AS jml_active FROM (SELECT id_contact FROM tb_status_change WHERE status_from != 'active' AND status_to = 'active' AND YEAR(created_at) = '$year' AND MONTH(created_at) = '$month' GROUP BY id_contact) t");
-        $rowActive['jml_active'] = $month;
+        $rowCount = $countActive->fetch_array(MYSQLI_ASSOC);
+        $rowActive['jml_active'] = $rowCount['jml_active'];
         $arrayActive[] = $rowActive;
     }
 
