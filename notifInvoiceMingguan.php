f0255925e03e82ba60ecc1b8d9e638d27e608571
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-01-24 10:20:14 +0700

    notif mingguan

--- notifInvoiceMingguan.php
+++ notifInvoiceMingguan.php
@@ -31,100 +31,109 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 
         if (date('Y-m-d') > date('Y-m-d', strtotime("+" . $invArray['termin_payment'] . " days", strtotime($invArray['date_invoice'])))) {
-            if ($days % 7 == 0) {
-                // if ($invArray['id_surat_jalan'] == '1633') {
-                $getTotalPayment = mysqli_query($conn, "SELECT SUM(amount_payment + potongan_payment + adjustment_payment) AS amount_total FROM tb_payment WHERE id_invoice = '$id_invoice'");
-                $rowPayment = $getTotalPayment->fetch_array(MYSQLI_ASSOC);
+            if ($days >= 16) {
 
-                $id_distributor = $invArray['id_distributor'];
-                $nomor_hp = $invArray['nomorhp'];
-                $nama = $invArray['nama'];
-                $no_invoice = $invArray['no_invoice'];
-                $sisaHutang = number_format($invArray['total_invoice'] - $rowPayment['amount_total'], 0, '.', ',');
+                if ($days % 7 == 0) {
+                    // if ($invArray['id_surat_jalan'] == '1633') {
+                    $getTotalPayment = mysqli_query($conn, "SELECT SUM(amount_payment + potongan_payment + adjustment_payment) AS amount_total FROM tb_payment WHERE id_invoice = '$id_invoice'");
+                    $rowPayment = $getTotalPayment->fetch_array(MYSQLI_ASSOC);
 
-                $getQontak = mysqli_query($conn, "SELECT * FROM tb_qontak WHERE id_distributor = '$id_distributor'");
-                $rowQontak = $getQontak->fetch_array(MYSQLI_ASSOC);
+                    $id_distributor = $invArray['id_distributor'];
+                    $nomor_hp = $invArray['nomorhp'];
+                    $nama = $invArray['nama'];
+                    $no_invoice = $invArray['no_invoice'];
+                    $sisaHutang = number_format($invArray['total_invoice'] - $rowPayment['amount_total'], 0, '.', ',');
 
-                $integration_id = $rowQontak['integration_id'];
-                $wa_token = $rowQontak['token'];
-                // echo json_encode($sisaHutang);
-                if ($sisaHutang > 0) {
-                    $response = ["message" => "Sudah waktunya", "days" => $days, "date_inv" => $invArray['date_invoice'], "toko" => $nama, "id_inv" => $id_invoice];
-                    echo json_encode($response);
-                    // if ($id_distributor == 2) {
-                    // $curl = curl_init();
+                    $getQontak = mysqli_query($conn, "SELECT * FROM tb_qontak WHERE id_distributor = '$id_distributor'");
+                    $rowQontak = $getQontak->fetch_array(MYSQLI_ASSOC);
 
-                    // curl_setopt_array($curl, array(
-                    //     CURLOPT_URL => 'https://service-chat.qontak.com/api/open/v1/broadcasts/whatsapp/direct',
-                    //     CURLOPT_RETURNTRANSFER => true,
-                    //     CURLOPT_ENCODING => '',
-                    //     CURLOPT_MAXREDIRS => 10,
-                    //     CURLOPT_TIMEOUT => 0,
-                    //     CURLOPT_FOLLOWLOCATION => true,
-                    //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    //     CURLOPT_CUSTOMREQUEST => 'POST',
-                    //     CURLOPT_POSTFIELDS => '{
-                    //             "to_number": "' . $nomor_hp . '",
-                    //             "to_name": "' . $nama . '",
-                    //             "message_template_id": "' . $template_id . '",
-                    //             "channel_integration_id": "' . $integration_id . '",
-                    //             "language": {
-                    //                 "code": "id"
-                    //             },
-                    //             "parameters": {
-                    //                 "body": [
-                    //                 {
-                    //                     "key": "1",
-                    //                     "value": "nama",
-                    //                     "value_text": "' . $nama . '"
-                    //                 },
-                    //                 {
-                    //                     "key": "2",
-                    //                     "value": "no_invoice",
-                    //                     "value_text": "' . $no_invoice . '"
-                    //                 },
-                    //                 {
-                    //                     "key": "3",
-                    //                     "value": "sisa",
-                    //                     "value_text": "' . $sisaHutang . '"
-                    //                 },
-                    //                 {
-                    //                     "key": "4",
-                    //                     "value": "jatuh_tempo",
-                    //                     "value_text": "' . $jatuhTempo . '"
-                    //                 }
-                    //                 ]
-                    //             }
-                    //             }',
-                    //     CURLOPT_HTTPHEADER => array(
-                    //         'Authorization: Bearer ' . $wa_token,
-                    //         'Content-Type: application/json'
-                    //     ),
-                    // ));
+                    $integration_id = $rowQontak['integration_id'];
+                    $wa_token = $rowQontak['token'];
+                    // echo json_encode($sisaHutang);
+                    if ($sisaHutang > 0) {
+                        $response = ["message" => "Sudah waktunya", "days" => $days, "date_inv" => $invArray['date_invoice'], "toko" => $nama, "id_inv" => $id_invoice];
+                        echo json_encode($response);
+                        // if ($id_distributor == 2) {
+                        // $curl = curl_init();
 
-                    // $response = curl_exec($curl);
+                        // curl_setopt_array($curl, array(
+                        //     CURLOPT_URL => 'https://service-chat.qontak.com/api/open/v1/broadcasts/whatsapp/direct',
+                        //     CURLOPT_RETURNTRANSFER => true,
+                        //     CURLOPT_ENCODING => '',
+                        //     CURLOPT_MAXREDIRS => 10,
+                        //     CURLOPT_TIMEOUT => 0,
+                        //     CURLOPT_FOLLOWLOCATION => true,
+                        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        //     CURLOPT_CUSTOMREQUEST => 'POST',
+                        //     CURLOPT_POSTFIELDS => '{
+                        //             "to_number": "' . $nomor_hp . '",
+                        //             "to_name": "' . $nama . '",
+                        //             "message_template_id": "' . $template_id . '",
+                        //             "channel_integration_id": "' . $integration_id . '",
+                        //             "language": {
+                        //                 "code": "id"
+                        //             },
+                        //             "parameters": {
+                        //                 "body": [
+                        //                 {
+                        //                     "key": "1",
+                        //                     "value": "nama",
+                        //                     "value_text": "' . $nama . '"
+                        //                 },
+                        //                 {
+                        //                     "key": "2",
+                        //                     "value": "no_invoice",
+                        //                     "value_text": "' . $no_invoice . '"
+                        //                 },
+                        //                 {
+                        //                     "key": "3",
+                        //                     "value": "sisa",
+                        //                     "value_text": "' . $sisaHutang . '"
+                        //                 },
+                        //                 {
+                        //                     "key": "4",
+                        //                     "value": "jatuh_tempo",
+                        //                     "value_text": "' . $jatuhTempo . '"
+                        //                 }
+                        //                 ]
+                        //             }
+                        //             }',
+                        //     CURLOPT_HTTPHEADER => array(
+                        //         'Authorization: Bearer ' . $wa_token,
+                        //         'Content-Type: application/json'
+                        //     ),
+                        // ));
+
+                        // $response = curl_exec($curl);
 
 
-                    // curl_close($curl);
+                        // curl_close($curl);
 
-                    // $res = json_decode($response, true);
+                        // $res = json_decode($response, true);
 
-                    // $status = $res['status'];
+                        // $status = $res['status'];
 
-                    // if ($status == "success") {
-                    //     $response = ["response" => 200, "status" => "ok", "message" => "Success notify customer"];
-                    //     echo json_encode($response);
-                    // } else {
-                    //     $response = ["response" => 200, "status" => "failed", "message" => "Failed notify customer. " . mysqli_error($conn), "detail" => mysqli_error($conn)];
-                    //     echo json_encode($response);
+                        // if ($status == "success") {
+                        //     $response = ["response" => 200, "status" => "ok", "message" => "Success notify customer"];
+                        //     echo json_encode($response);
+                        // } else {
+                        //     $response = ["response" => 200, "status" => "failed", "message" => "Failed notify customer. " . mysqli_error($conn), "detail" => mysqli_error($conn)];
+                        //     echo json_encode($response);
+                        // }
+                    }
                     // }
+                } else {
+                    $response = ["message" => "Belum kelipatan 7 hari", "days" => $days, "date_inv" => $invArray['date_invoice'], "toko" => $nama, "id_inv" => $id_invoice];
+                    echo json_encode($response);
                 }
-                // }
+                // echo $jatuhTempo;
             } else {
-                $response = ["message" => "Belum waktunya", "days" => $days, "date_inv" => $invArray['date_invoice'], "toko" => $nama, "id_inv" => $id_invoice];
+                $response = ["message" => "Belum 16 hari", "days" => $days, "date_inv" => $invArray['date_invoice'], "toko" => $nama, "id_inv" => $id_invoice];
                 echo json_encode($response);
             }
-            // echo $jatuhTempo;
+        } else {
+            $response = ["message" => "Bukan jatem", "days" => $days, "date_inv" => $invArray['date_invoice'], "toko" => $nama, "id_inv" => $id_invoice];
+            echo json_encode($response);
         }
     }
 }
