88ec25a68d5bca55d5ef632c91ec1bc7d6572073
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-02-19 23:59:01 +0700

    forward ultah

--- birthday.php
+++ birthday.php
@@ -28,7 +28,8 @@ foreach ($transArray as $arr) {
 
         $nomor_forward = '';
         if ($id_distributor == 1) {
-            $nomor_forward = '6282143365316';
+            // $nomor_forward = '6282143365316';
+            $nomor_forward = '6282233392197';
         } else if ($id_distributor == 2) {
             $nomor_forward = '6282111110710';
         } else {
