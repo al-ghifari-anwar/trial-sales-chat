04764027a94ebe339056bf83a293e105d8457709
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-03-12 10:50:00 +0700

    cek tukang timeout

--- tukang.php
+++ tukang.php
@@ -28,11 +28,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $key = isset($_GET['key']) ? $_GET['key'] : '';
             $id_city = $_GET['c'];
 
-            $result = mysqli_query($conn, "SELECT * FROM tb_tukang JOIN tb_skill ON tb_skill.id_skill = tb_tukang.id_skill WHERE (nama LIKE '%$key%' OR nomorhp LIKE '%$key%') AND tb_tukang.id_city = '$id_city' ");
+            $result = mysqli_query($conn, "SELECT * FROM tb_tukang JOIN tb_skill ON tb_skill.id_skill = tb_tukang.id_skill WHERE (nama LIKE '%$key%' OR nomorhp LIKE '%$key%') AND tb_tukang.id_city = '$id_city' ORDER BY tb_tukang.created_at DESC LIMIT 25");
         } else {
             $key = isset($_GET['key']) ? $_GET['key'] : '';
             $id_dist = $_GET['dst'];
-            $result = mysqli_query($conn, "SELECT * FROM tb_tukang JOIN tb_skill ON tb_skill.id_skill = tb_tukang.id_skill JOIN tb_city ON tb_city.id_city = tb_tukang.id_city WHERE (nama LIKE '%$key%' OR nomorhp LIKE '%$key%') AND id_distributor = '$id_dist'");
+            $result = mysqli_query($conn, "SELECT * FROM tb_tukang JOIN tb_skill ON tb_skill.id_skill = tb_tukang.id_skill JOIN tb_city ON tb_city.id_city = tb_tukang.id_city WHERE (nama LIKE '%$key%' OR nomorhp LIKE '%$key%') AND id_distributor = '$id_dist' ORDER BY tb_tukang.created_at DESC LIMIT 25");
         }
 
         while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
