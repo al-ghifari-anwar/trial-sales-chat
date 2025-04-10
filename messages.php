e5735e0e53b8e360b6304aca2e87051a3628e41d
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-04-10 15:34:33 +0700

    Fix message for tkp

--- messages.php
+++ messages.php
@@ -103,7 +103,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_POST['message_body'])) {
         $integration_id = $rowQontak['integration_id'];
         if ($rowUserData['id_distributor'] == 4) {
-            $nama = $getContact['store_owner'];
+            $nama = $rowContact['store_owner'];
             $wa_token = 'lzKKjup6a2_fKtoDZ0KJO4pFURhKte5eVKzeD9Ect0A';
             $integration_id = $rowUserData['integration_id'];
             $template_id = '117cfa6d-772d-480f-97e5-cbf9ad6acb74';
