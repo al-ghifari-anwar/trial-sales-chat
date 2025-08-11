6a9c2f8fa6acbb788890a053c52ee82588e41547
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-08-11 09:39:48 +0700

    Cek invoice monday

--- notifInvoiceMonday.php
+++ notifInvoiceMonday.php
@@ -113,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     $response = ["response" => 200, "status" => "ok", "message" => "Success notify customer"];
                     echo json_encode($response);
                 } else {
-                    $response = ["response" => 200, "status" => "failed", "message" => "Failed notify customer. " . mysqli_error($conn), "detail" => mysqli_error($conn)];
+                    $response = ["response" => 200, "status" => "failed", "message" => "Failed notify customer. " . mysqli_error($conn), "detail" => mysqli_error($conn), "qontak" => $res];
                     echo json_encode($response);
                 }
                 // }
