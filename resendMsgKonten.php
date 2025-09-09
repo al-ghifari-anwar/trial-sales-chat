a3c4821fd293940f5837f6fd6e3df76ecfcff90d
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-09-09 13:51:16 +0700

    Send img msg

--- resendMsgKonten.php
+++ resendMsgKonten.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     $getMsgKonten = mysqli_query($conn, "SELECT * FROM tb_msg_konten WHERE is_sent = 0");
 
-    while ($rowMsgKonten = $getMsgKonten->fetch_array($getMsgKonten)) {
+    while ($rowMsgKonten = $getMsgKonten->fetch_array(MYSQLI_ASSOC)) {
         $updated_at = date('Y-m-d H:i:s');
         $id_msg_konten = $rowMsgKonten['id_msg_konten'];
         $id_contact = $rowMsgKonten['id_contact'];
