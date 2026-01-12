bed701f59a72bd8181996da8471e5f02e26b82b3
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-01-12 11:52:05 +0700

    fix counter

--- absenSales.php
+++ absenSales.php
@@ -1,4 +1,7 @@
 <?php
+// error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+include_once("config.php");
+date_default_timezone_set('Asia/Jakarta');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['id_gudang'])) {
