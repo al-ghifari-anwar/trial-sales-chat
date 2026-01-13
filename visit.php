e363515f617f5f5a8124a917e5dbf2910ca1b950
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-01-13 12:47:04 +0700

    fix visit passive

--- visit.php
+++ visit.php
@@ -729,6 +729,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
                 curl_close($curl);
 
+                echo $response;
+
                 $getRenvis = mysqli_query($conn, "UPDATE tb_rencana_visit SET is_visited = 1, visit_date = '$visitDate' WHERE id_contact = '$id_contact' AND type_rencana = '$type_renvi'");
             } else if ($type_renvi == 'tagih_mingguan' || $type_renvi == 'weekly') {
                 $getRenvis = mysqli_query($conn, "UPDATE tb_rencana_visit SET is_visited = 1, visit_date = '$visitDate' WHERE id_contact = '$id_contact' AND type_rencana = 'tagih_mingguan'");
