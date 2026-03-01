221a2ade253e5749efc6b330204cf9eb06eb0431
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-03-02 02:50:32 +0700

    Reminder voucehr

--- notifTagihan.php
+++ notifTagihan.php
@@ -24,23 +24,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $nama = $salesArray['full_name'];
         $id_distributor = $salesArray['id_distributor'];
 
-        $getQontak = mysqli_query($conn, "SELECT * FROM tb_qontak WHERE id_distributor = '$id_distributor'");
-        $rowQontak = $getQontak->fetch_array(MYSQLI_ASSOC);
+        // $getQontak = mysqli_query($conn, "SELECT * FROM tb_qontak WHERE id_distributor = '$id_distributor'");
+        // $rowQontak = $getQontak->fetch_array(MYSQLI_ASSOC);
 
-        $integration_id = $rowQontak['integration_id'];
-        $wa_token = $rowQontak['token'];
+        // $integration_id = $rowQontak['integration_id'];
+        // $wa_token = $rowQontak['token'];
 
-        if ($nomor_hp == "6287774436555" || $nomor_hp == "6281808152028") {
-            $message = "Laporan rekap piutang: https://order.topmortarindonesia.com/wh-tagihan?c=0&dst=$id_distributor";
-            $msgStatus = "All City";
-            // echo $message;
-        } else {
-            $message = "Laporan rekap piutang: https://order.topmortarindonesia.com/wh-tagihan?c=$id_city&dst=$id_distributor";
-            $msgStatus = "Specific City";
-            // echo $message;
-        }
+        $getHaloai = mysqli_query($conn, "SELECT * FROM tb_haloai WHERE id_distributor = '$id_distributor'");
+        $rowHaloai = $getHaloai->fetch_array(MYSQLI_ASSOC);
+        $wa_token = $rowHaloai['token_haloai'];
+        $business_id = $rowHaloai['business_id_haloai'];
+        $channel_id = $rowHaloai['channel_id_haloai'];
+        $template = 'info_meeting_baru';
+
+        // if ($nomor_hp == "6287774436555" || $nomor_hp == "6281808152028") {
+        //     $message = "Laporan rekap piutang: https://order.topmortarindonesia.com/wh-tagihan?c=0&dst=$id_distributor";
+        //     $msgStatus = "All City";
+        // echo $message;
+        // } else {
+        $message = "Laporan rekap piutang: https://order.topmortarindonesia.com/wh-tagihan?c=$id_city&dst=$id_distributor";
+        $msgStatus = "Specific City";
+        // echo $message;
+        // }
 
         if ($nomor_hp != "6287774436555" || $nomor_hp != "6281808152028" || $nomor_hp != "6281235834111" || $nomor_hp != "6281952581199" || $nomor_hp != "6285335631783" || $nomor_hp != "6281808152028" || $nomor_hp != "6287757904850") {
+
+            $haloaiPayload = [
+                'activate_ai_after_send' => false,
+                'channel_id' => $channel_id,
+                'fallback_template_message' => $template,
+                'fallback_template_variables' => [
+                    $nama['nama'],
+                    trim(preg_replace('/\s+/', ' ', $message)),
+                    "PT Top Mortar Indonesia",
+                ],
+                'phone_number' => $nomor_hp,
+                'text' => trim(preg_replace('/\s+/', ' ', $message)),
+            ];
 
             // $curl = curl_init();
 
@@ -91,17 +111,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
             // curl_close($curl);
 
-            // $res = json_decode($response, true);
-            // $status = $res['status'];
-            // $id = $data['data']['id'];
+            $curl = curl_init();
 
-            // if ($status == "success") {
-            //     $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim laporan tagihan!", "detail" => $msgStatus, "body" => $message];
-            //     echo json_encode($response);
-            // } else {
-            //     $response = ["response" => 200, "status" => "failed", "message" => "Gagal mengirim laporan tagihan!", "id" => $id, "target" => $salesArray['username'] . " - " . $nomor_hp];
-            //     echo json_encode($response);
-            // }
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://www.haloai.co.id/api/open/channel/whatsapp/v1/sendMessageByPhoneSync',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode($haloaiPayload),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: Bearer ' . $wa_token,
+                    'X-HaloAI-Business-Id: ' . $business_id,
+                    'Content-Type: application/json'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $res = json_decode($response, true);
+            $status = $res['status'];
+
+            if ($status == "success") {
+                $response = ["response" => 200, "status" => "ok", "message" => "Berhasil mengirim laporan tagihan!", "detail" => $msgStatus, "body" => $message];
+                echo json_encode($response);
+            } else {
+                $response = ["response" => 200, "status" => "failed", "message" => "Gagal mengirim laporan tagihan!", "target" => $salesArray['username'] . " - " . $nomor_hp];
+                echo json_encode($response);
+            }
         }
     }
 }
