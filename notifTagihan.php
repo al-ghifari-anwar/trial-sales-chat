0298e2a31e0a83cc0f15149dbf095f8d6b3d2650
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-03-02 06:03:53 +0700

    Notif tagihan sales

--- notifTagihan.php
+++ notifTagihan.php
@@ -1,6 +1,8 @@
 <?php
 
-// error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 include_once("config.php");
 date_default_timezone_set('Asia/Jakarta');
 
