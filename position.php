09b94b265fd9847429d56ae9d0e61fa09218d1cf
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-03-12 11:07:15 +0700

    Get detail user position and date

--- position.php
+++ position.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 } else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['id_user'])) {
         $id_user = $_GET['id_user'];
-        $date = isset($_GET['date']) ? $_GET['date'] : null;
+        $date = $_GET['date'];
 
         $user = mysqli_query($conn, " SELECT * FROM tb_user WHERE id_user = '$id_user' ")->fetch_array(MYSQLI_ASSOC);
 
