4b8016836b8996d50d4fc5df38a7e16f910f187e
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-03-27 05:57:17 +0700

    Auto remove ungoal contact store

--- autoRemoveUngoal.php
+++ autoRemoveUngoal.php
@@ -8,10 +8,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     while ($rowContact = $getContacts->fetch_array(MYSQLI_ASSOC)) {
         $id_contact = $rowContact['id_contact'];
-        $visit = mysqli_query($conn, " SELECT COUNT(*) AS jml_visit FROM tb_visit WHERE id_contact = $id_contact ");
+        $id_distributor = $rowContact['id_distributor'];
+        $nama_city_buangan = trim(preg_replace("/\\d+/", "", $rowContact['nama_city'])) . " X";
+        $visit = mysqli_query($conn, " SELECT COUNT(*) AS jml_visit FROM tb_visit WHERE id_contact = $id_contact ")->fetch_array(MYSQLI_ASSOC);
+
+        $cityBuangan = mysqli_query($conn, " SELECT * FROM tb_city WHERE nama_city = '$nama_city_buangan' AND id_distributor = '$id_distributor' ")->fetch_array(MYSQLI_ASSOC);
 
         if ($visit['jml_visit'] >= 8) {
-            // 
+            if ($cityBuangan) {
+                $id_city_buangan = $cityBuangan['id_city'];
+                $updateContact = mysqli_query($conn, "UPDATE tb_contact SET id_city = '$id_city_buangan' WHERE id_contact = '$id_contact'");
+
+                if (!$updateContact) {
+                    $response = ["response" => 4200, "status" => "failed", "message" => "Gagal pindah toko!"];
+                    echo json_encode($response);
+                } else {
+                    $response = ["response" => 200, "status" => "ok", "message" => "Toko " . $rowContact['nama'] . " Berhasil dipindah! "];
+                    echo json_encode($response);
+                }
+            }
         }
     }
 }
