4fcca681fe7e4572781db249680403fa9adc3753
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-09-10 13:19:35 +0700

    step renvi

--- stepRenvi.php
+++ stepRenvi.php
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             // Hit OSRM untuk jarak
             $coordinateString = implode(';', $coordinates);
 
-            $url = 'https://router.project-osrm.org/table/v1/driving/' . $coordinateString . '?sources=0&destinations=' . implode(';', range(1, count($renvis))) . '&annotations=duration,distance';
+            $url = 'http://router.project-osrm.org/table/v1/driving/' . $coordinateString . '?sources=0&destinations=' . urlencode(implode(';', range(1, count($renvis)))) . '&annotations=duration,distance';
 
             $curl = curl_init();
 
